049d70e0de52e59d79242928bafc89f65be879d5
riceman225 <riceman225@gmail.com>
2013-02-28 15:42:40 +0800

    登陆后首页 ing

--- js/feeds.js
+++ js/feeds.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
 
+//Show Feed-Actions
+
+	$(".feeds-item").mouseenter(function(){
+		$(this).addClass("item-activated");
+		$(".item-activated .feeds-actions").css("visibility","visible");
+	});
+
+
+	$(".feeds-item").mouseleave(function(){
+		$(".item-activated .feeds-actions").css("visibility","hidden");
+		$(this).removeClass("item-activated");
+	});
+	
+//New Feeds Click
+	$(".new-feeds-bar").click(function(){
+		$(this).hide();
+		$(".new-feeds").show();
+	});
+
+
 //坑爹的图标切换我怎么要用这么蠢的办法。。。。
 
-	$(".feeds-reply-container").mouseenter(function(){
+	$(".feeds-comment-container").mouseenter(function(){
 		$(this).addClass("feeds-activating");
-		$(".feeds-activating .icon-arrow-left").removeClass("icon-grey");
+		$(".feeds-activating .ui-icon-comment").addClass("ui-icon-comment-hover");
+		$(".feeds-activating .ui-icon-comment-hover").removeClass("ui-icon-comment");
 	});
-	
-	$(".feeds-reply-container").mouseleave(function(){
-		$(".feeds-activating .icon-arrow-left").addClass("icon-grey");
+
+
+	$(".feeds-comment-container").mouseleave(function(){
+		$(".feeds-activating .ui-icon-comment-hover").addClass("ui-icon-comment");
+		$(".feeds-activating .ui-icon-comment").removeClass("ui-icon-comment-hover");
 		$(this).removeClass("feeds-activating");
 	});
-	
-	
-	
+
+
 	$(".feeds-share-container").mouseenter(function(){
 		$(this).addClass("feeds-activating");
 		$(".feeds-activating .icon-share-alt").removeClass("icon-grey");
@@ -36,14 +58,19 @@ $(document).ready(function(){
 		$(this).removeClass("feeds-activating");
 	});
 	
+
 //点击收藏	
-	$(".feeds-favorite-container").click(function(){
-		$(this).addClass("feeds-favorited");
-		$(".feeds-favorited i").removeClass("icon-grey icon-star-empty");
-		$(".feeds-favorited i").addClass("icon-star");
-		$(".feeds-activating .notfavorited").css("display","none");
-		$(".feeds-activating .favorited").css("display","inline");
-		
+	$(".feeds-favorite-button").click(function(){
+		$(".feeds-activating .feeds-favorite-button-done").removeClass("hide");
+		$(".feeds-activating .feeds-favorite-button").addClass("hide");
+	});
+
+
+
+//取消收藏
+	$(".feeds-favorite-button-done").click(function(){
+		$(".feeds-activating .feeds-favorite-button-done").addClass("hide");
+		$(".feeds-activating .feeds-favorite-button").removeClass("hide");
 	});
 
 
