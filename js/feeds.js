e8f959d367027fbbed6a5c448d0992037895638d
riceman225 <riceman225@gmail.com>
2013-03-13 01:02:24 +0800

    登陆后首页 添加转发功能，简化部分js

--- js/feeds.js
+++ js/feeds.js
@@ -7,22 +7,11 @@ $(document).ready(function(){
 
 
 
-
 //Show Feed-Actions
-
-	$(".feeds-item").mouseenter(function(){
-		$(this).addClass("item-activated");
-		$(".feeds-actions",this).css("visibility","visible");
-	});
-
-
-	$(".feeds-item").mouseleave(function(){
-		if ($(".feed-replies", this).is(':visible')) {
-			$(this).removeClass("item-activated");
-		} else {
-			$(".feeds-actions", this).css("visibility","hidden");
-			$(this).removeClass("item-activated");
-		};
+	$(".feeds-actions").hide();
+	$(".feeds-item").hover(function(){
+		$(this).toggleClass("item-activated");
+		$(".feeds-actions",this).toggle();
 	});
 	
 	
@@ -34,64 +23,41 @@ $(document).ready(function(){
 	});
 
 
-//坑爹的图标切换我怎么要用这么蠢的办法。。。。
+//坑爹的Feeds-Action图标切换
+	$(".feeds-comment-container").hover(function(){
+		$("i",this).toggleClass("ui-icon-comment-hover");
+	});
 
-	$(".feeds-comment-container").mouseenter(function(){
-		$(this).addClass("feeds-activating");
-		$(".feeds-activating .ui-icon-comment").addClass("ui-icon-comment-hover");
-		$(".feeds-activating .ui-icon-comment-hover").removeClass("ui-icon-comment");
+	$(".feeds-share-container").hover(function(){
+		$("i",this).toggleClass("icon-grey");
+	});
+
+	$(".feeds-favorite-button").hover(function(){
+		if ($(this).hasClass("hide")){
+			return;
+		} else {
+			$("i",this).toggleClass("icon-grey");
+		};
 	});
 
 
-	$(".feeds-comment-container").mouseleave(function(){
-		$(".feeds-activating .ui-icon-comment-hover").addClass("ui-icon-comment");
-		$(".feeds-activating .ui-icon-comment").removeClass("ui-icon-comment-hover");
-		$(this).removeClass("feeds-activating");
+//点击收藏
+	$(".feeds-favorite-button").click(function(event){
+		event.stopImmediatePropagation();
+		$(".item-activated .feeds-favorite-button").addClass("hide");
+		$(".item-activated .feeds-favorite-button-done").removeClass("hide");
 	});
-
-
-	$(".feeds-share-container").mouseenter(function(){
-		$(this).addClass("feeds-activating");
-		$(".feeds-activating .icon-share-alt").removeClass("icon-grey");
-	});
-	
-	$(".feeds-share-container").mouseleave(function(){
-		$(".feeds-activating .icon-share-alt").addClass("icon-grey");
-		$(this).removeClass("feeds-activating");
-	});
-	
-	
-	
-	$(".feeds-favorite-container").mouseenter(function(){
-		$(this).addClass("feeds-activating");
-		$(".feeds-activating .icon-star-empty").removeClass("icon-grey");
-	});
-	
-	$(".feeds-favorite-container").mouseleave(function(){
-		$(".feeds-activating .icon-star-empty").addClass("icon-grey");
-		$(this).removeClass("feeds-activating");
-	});
-	
-
-//点击收藏	
-	$(".feeds-favorite-button").click(function(){
-		$(".feeds-activating .feeds-favorite-button-done").removeClass("hide");
-		$(".feeds-activating .feeds-favorite-button").addClass("hide");
-	});
-
-
 
 //取消收藏
-	$(".feeds-favorite-button-done").click(function(){
-		$(".feeds-activating .feeds-favorite-button-done").addClass("hide");
-		$(".feeds-activating .feeds-favorite-button").removeClass("hide");
+	$(".feeds-favorite-button-done").click(function(event){
+		event.stopImmediatePropagation();
+		$(".item-activated .feeds-favorite-button-done").addClass("hide");
+		$(".item-activated .feeds-favorite-button").removeClass("hide");
 	});
 
 
 // Seperate Single Feed
 // If the first feed is clicked
-
-
 	$(".feeds-item:first").click(function(){
 		$(".item-activated").toggleClass("first-feed-item-click");
 	});
@@ -102,8 +68,10 @@ $(document).ready(function(){
 		$(".item-activated").toggleClass("feeds-item-click");
 		$(".item-activated").prev(".feeds-item").toggleClass("sep-prev-feed");
 		$(".item-activated").next(".feeds-item").toggleClass("sep-next-feed");
-		$(".item-activated .feed-replies").slideToggle(200);	
+		$(".item-activated .feed-replies").slideToggle(200);
 	});
+	
+	
 	
 //点击feed中其他元素不要触发上面这个feed click事件
    $(".feeds-item-header > a, .feed-replies, .feeds-item-content > .feeds-text > #user-id").click(function(e) {
@@ -111,18 +79,30 @@ $(document).ready(function(){
    });
 
 
+//Share-Feed-Button Click事件
+	$(".feeds-share-button").click(function(event){
+		event.stopImmediatePropagation();
+		var $ShareModalTitle = $(".item-activated .feeds-item-header .username").text();
+		var $OriginalHeader = $(".item-activated .feeds-item-header").html();
+		var $OriginalContent = $(".item-activated .feeds-item-content").html();
+		
+		$("#ShareModal .original-feed-header").html($OriginalHeader);
+		$("#ShareModal .original-feed-content").html($OriginalContent);
+		$("#ShareModal").modal();
+	});
 
 
 // Reply-box Textarea 高度自适应
 	$('.mini-reply-box').tah({
 		moreSpace:1,   //输入框底部预留的空白, 默认10, 单位像素
-		maxHeight:100,  //指定Textarea的最大高度, 默认600, 单位像素
+		maxHeight:200,  //指定Textarea的最大高度, 默认600, 单位像素
 		animateDur:200  //调整高度时的动画过渡时间, 默认200, 单位毫秒
 	});
 	
+	
 // 回复评论
 	$('.replies-actions').click(function(){
-		var userName = $(this).parents(".replies-item").find(".username").html();
+		var userName = $(this).parents(".replies-item").find(".username").text();
 		$(".item-activated textarea").text("@" + userName + " ");
 	});
 	
--- js/speak.js
+++ js/speak.js
@@ -1,23 +1,20 @@
 $(document).ready(function(){
 
-
+//Speak-Box一个纯粹为了炫的效果。。。
 	$('#mini-speak-box').focus(function(){
         $(".mini-speak-box").addClass("hide");
         $(".rich-speak-box").removeClass("hide");
-        $(this).blur()
+        $(this).blur();
         $(".rich-speak-text").focus();
     });
-
-
-	$('.rich-speak-text').blur(function(){
-		if ($('.rich-speak-text').val() === "") {
-			$(".mini-speak-box").removeClass("hide");
-	        $(".rich-speak-box").addClass("hide");
-		}
+	
+	$(document).click(function(e){
+	    if($(e.target).is('.rich-speak-box, .rich-speak-box *') || $('.rich-speak-text').val() !== "")return;
+		$(".mini-speak-box").removeClass("hide");
+	    $(".rich-speak-box").addClass("hide");
 	});
 
-	
-	
+
 
 //左边 我说 的wordcount
 	var maxLength = 225; // max number of allowed characters
@@ -27,7 +24,6 @@ $(document).ready(function(){
 	});
 	
 //replybox的wordcount
-
 	$('.mini-reply-box').keyup(function() {
 		$(this).val($(this).val().substr(0, maxLength));
 		$('.reply-word-counter').text(maxLength - $(this).val().length);
